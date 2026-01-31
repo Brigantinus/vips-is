170ffc42040bb587475832572d21f2054fde54d6
Brigantinus <mhinters@gmail.com>
2026-01-31 16:55:47 +0100

    Add NotFoundExceptionMapper.

--- src/main/java/cc/vips_is/rest/mapper/CustomExceptionMapper.java
+++ src/main/java/cc/vips_is/rest/mapper/CustomExceptionMapper.java
@@ -1,9 +1,10 @@
 package cc.vips_is.rest.mapper;
 
+import cc.vips_is.service.image.exceptions.ImageProcessingException;
 import cc.vips_is.service.image.exceptions.InvalidParameterException;
 import cc.vips_is.service.storage.exceptions.ImageNotFoundException;
-import cc.vips_is.service.image.exceptions.ImageProcessingException;
 import cc.vips_is.service.storage.exceptions.StorageException;
+import jakarta.ws.rs.NotFoundException;
 import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
@@ -16,7 +17,18 @@ import lombok.extern.slf4j.Slf4j;
 public class CustomExceptionMapper {
 
     @Provider
-    public static class NotFoundMapper implements ExceptionMapper<ImageNotFoundException> {
+    public static class NotFoundExceptionMapper implements ExceptionMapper<NotFoundException> {
+
+        @Override
+        public Response toResponse(NotFoundException exception) {
+            return Response.status(Response.Status.NOT_FOUND)
+                    .entity("Resource not found")
+                    .build();
+        }
+    }
+
+    @Provider
+    public static class ImageNotFoundExceptionMapper implements ExceptionMapper<ImageNotFoundException> {
         @Context UriInfo uriInfo;
         @Override
         public Response toResponse(ImageNotFoundException e) {
@@ -25,7 +37,7 @@ public class CustomExceptionMapper {
     }
 
     @Provider
-    public static class InvalidParameterMapper implements ExceptionMapper<InvalidParameterException> {
+    public static class InvalidParameterExceptionMapper implements ExceptionMapper<InvalidParameterException> {
         @Context UriInfo uriInfo;
         @Override
         public Response toResponse(InvalidParameterException e) {
@@ -34,7 +46,7 @@ public class CustomExceptionMapper {
     }
 
     @Provider
-    public static class StorageMapper implements ExceptionMapper<StorageException> {
+    public static class StorageExceptionMapper implements ExceptionMapper<StorageException> {
         @Context UriInfo uriInfo;
         @Override
         public Response toResponse(StorageException e) {
@@ -43,7 +55,7 @@ public class CustomExceptionMapper {
     }
 
     @Provider
-    public static class SecurityMapper implements ExceptionMapper<SecurityException> {
+    public static class SecurityExceptionMapper implements ExceptionMapper<SecurityException> {
         @Context UriInfo uriInfo;
         @Override
         public Response toResponse(SecurityException e) {
@@ -52,7 +64,7 @@ public class CustomExceptionMapper {
     }
 
     @Provider
-    public static class ProcessingMapper implements ExceptionMapper<ImageProcessingException> {
+    public static class ProcessingExceptionMapper implements ExceptionMapper<ImageProcessingException> {
         @Context UriInfo uriInfo;
         @Override
         public Response toResponse(ImageProcessingException e) {
